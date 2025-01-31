61aec234921cdf5ae6586fcb619f93e481c3a4fe
marwan.salman <markamel@publicisgroupe.net>
2025-01-31 23:35:51 +0100

    enhance in genericErrorMessage

--- src/main/java/errors/ErrorMessage.java
+++ src/main/java/errors/ErrorMessage.java
@@ -3,9 +3,16 @@ package errors;
 import static java.lang.System.err;
 
 public class ErrorMessage {
+    private boolean print = true; // Default value is false
 
     public String genericErrorMessage(String message) {
-        err.println(message);
-        return message;
+        return genericErrorMessage(message, print); // Default to 'false' if no print parameter is provided
+    }
+
+    public String genericErrorMessage(String message, boolean print) {
+        if (print) {
+            err.println(message);  // Print the message only if the flag is true
+        }
+        return message;  // Return the message as usual
     }
 }
